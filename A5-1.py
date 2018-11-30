64a91a37557675ff785d8c12ab9f26dd1434f6b0
Araham Kong <abrahamkong@github.com>
2018-11-30 10:48:16 -0800

    get X, Y, and Z

--- A5-1.py
+++ A5-1.py
@@ -7,6 +7,12 @@ type(key)
 # Convert to Binary
 key = format(key, '#064b')
 # key = bin(key)
-print(key)
+print("This is your number in binary: " + key)
 
-key.x = 
+key_x = key[2:21]
+key_y = key[21:-25]
+key_z = key[-25:]
+
+print("X: " + key_x)
+print("Y: " + key_y)
+print("Z: " + key_z) 
